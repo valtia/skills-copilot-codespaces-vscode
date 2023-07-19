26a6f2e9526e3bf64608e33ad20cef8e64dbf1ea
valtia <129421602+valtia@users.noreply.github.com>
2023-07-19 23:44:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  var member = {
+    name: "John Doe",
+    age: 30,
+    calculateNumbers: function(var1, var2) {
+      return var1 + var2;
+    }
+  };
+  return member;
+}
